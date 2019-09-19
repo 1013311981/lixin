69fc72b9e70d33d191fb0cde3721532c85610e5f
lixin <lixin1>
2019-09-19 18:11:59 +0800

    websocket学习

---
+++ websocket/src/main/java/top/rurenyinshui/websocket/config/WebSocketConfig.java
@@ -0,0 +1,52 @@
+package top.rurenyinshui.websocket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+import top.rurenyinshui.websocket.factory.WebSocketDecoratorFactory;
+import top.rurenyinshui.websocket.handler.PrincipalHandshakeHandler;
+
+/**
+ * @program: lixin
+ * @author: LiXin
+ * @create: 2019-09-19 17:28
+ **/
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private WebSocketDecoratorFactory webSocketDecoratorFactory;
+
+    @Autowired
+    private PrincipalHandshakeHandler principalHandshakeHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        /**
+         * myUrl表示 你前端到时要对应url映射
+         */
+        registry.addEndpoint("/myUrl")
+                .setAllowedOrigins("*")
+                .setHandshakeHandler(principalHandshakeHandler)
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        /**
+         * queue 点对点
+         * topic 广播
+         * user 点对点前缀
+         */
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.addDecoratorFactory(webSocketDecoratorFactory);
+
+    }
+}
